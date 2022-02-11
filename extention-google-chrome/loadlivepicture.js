3421e343c43b18caf53215d63eff253dc0c383e9
Code-it <code.it.ytb@gmail.com>
2022-02-11 09:19:52 +0100

    Update loadlivepicture.js

--- extention-google-chrome/loadlivepicture.js
+++ extention-google-chrome/loadlivepicture.js
@@ -1,10 +1,4 @@
-import { islive } from 'https://code-it-ytb.github.io/extention-google-chrome/islive.js'
+var islive = "True"
+var livetitle = ""
 
-if (islive == "True") {
-  $("#pageContent #link img").attr("src", "https://i.ytimg.com/vi/" + islive.liveid + "/mqdefault.jpg");
-  $("#pageContent #link a").attr("href", "https://youtu.be/" + islive.liveid);
-  $("#pageContent #link title").text(islive.livetitle);
-} else {
-  $("#pageContent #link img").attr("src", "https://code-it-ytb.github.io/extention-google-chrome/offline.jpg");
-  $("#pageContent #link title").text("Pas de live ...");
-}
+document.getElementById("livetitlespan").innerHTML=livetitle;
