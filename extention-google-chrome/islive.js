c277dbdaafcb9b578a4eaa429bec89a51d426e4b
Code-it <code.it.ytb@gmail.com>
2022-03-11 13:06:40 +0100

    Update islive.js

--- extention-google-chrome/islive.js
+++ extention-google-chrome/islive.js
@@ -1,2 +1,7 @@
-islive = "True"
-liveid = "jNQXAC9IVRw"
+islive = "False"
+liveid = None
+napostrophe = "n'"
+pas = "pas"
+
+document.getElementById("napostrophe").innerHTML=napostrophe;
+document.getElementById("pas").innerHTML=pas;
